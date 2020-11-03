3f5ee5b0c52ceb6d758f920aacb5d2d42d10aabd
dmutoni <mdenyse15@gmail.com>
2020-11-03 15:41:00 +0200

    database-configuration

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql');
 
 let mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'dms'
+    host: 'sql9.freemysqlhosting.net',
+    user: 'sql9374228',
+    password: '3pyCJb6V7P',
+    database: 'sql9374228',
+    // port: 3306
 })
 mysqlConnection.connect((err) => {
     if (!err) {
-        console.log("DB connection successful");
+        console.log(`DB connection successful`);
     } else {
         console.log("DB connection failed \n Error : " + JSON.stringify(err, undefined, 2));
     }
